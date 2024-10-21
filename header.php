bf8a145b9571b71e85d14046ac3e940b2e61cbdb
jessica-anne21 <jesshar2108@gmail.com>
2024-10-21 19:03:27 +0700

    edit

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>School Managemnt System</title>
+  <title>School Management System</title>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
